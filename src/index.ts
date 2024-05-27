3c0b6bfd44601591517dfe03ca9359d32fa7325f
Dawson <hello@dawson.gg>
2024-05-26 23:04:13 -0400

    feat: add onto the KFAPI class with some more methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+class KeyforgeAPI {
+  private static apiKey: string | null = null;
+  private static accountToken: string | null = null;
+
+  public static setCredentials(apiKey: string, accountToken?: string): void {
+    KeyforgeAPI.apiKey = apiKey;
+    KeyforgeAPI.accountToken = accountToken || null;
+  }
+
+  public static getCredentials(): {
+    apiKey: string | null;
+    accountToken: string | null;
+  } {
+    return {
+      apiKey: KeyforgeAPI.apiKey,
+      accountToken: KeyforgeAPI.accountToken,
+    };
+  }
+}
+
+export default KeyforgeAPI;
