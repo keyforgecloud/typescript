aed555d8164212bcfa9312e3e01abb7ccb4a3bca
Dawson <hello@dawson.gg>
2024-05-27 14:54:17 -0400

    fix: changed key schema to be correct

--- src/types/Key.ts
+++ src/types/Key.ts
@@ -1,12 +1,12 @@
 ﻿export interface Key {
   id: string;
   name: string;
+  enabled: boolean;
   ownerId: string;
   apiOwnerId: string;
   apiId: string;
   permissions: string[];
   meta: Record<string, any>;
   lastUsed: string;
-  timesUsed: number;
   createdAt: string;
 }
